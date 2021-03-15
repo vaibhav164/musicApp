b3683e655108fa5df499d24fc43b41981c2ec6ba
vai1641996 <vai1641996@gmail.com>
2021-03-15 09:44:50 +0530

    mede Login page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput,Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,22 +7,38 @@ import { NavigationContainer } from '@react-navigation/native';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'powderblue' }}>
-      <Text>Home!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#e4bad4' }}>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Login."
+          placeholderTextColor="#000"
+          onChangeText={(email) => setEmail(email)}
+            />
+        </View>
+        <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#000"
+          onChangeText={(email) => setEmail(email)}
+            />
+        </View>
+        <Button title="Login"/>
     </View>
-  );z
+  ); z
 }
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: '#f48b29' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f48b29' }}>
       <Text>Settings!</Text>
     </View>
   );
 }
 function SettingsLog() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'#cdc733' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#cdc733' }}>
       <Text>Log!</Text>
     </View>
   );
@@ -44,7 +60,7 @@ export default function App() {
               iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
             } else if (route.name === 'Settings') {
               iconName = focused ? 'ios-list' : 'ios-list-outline';
-            }else if (route.name === 'Log') {
+            } else if (route.name === 'Log') {
               iconName = focused ? 'book' : 'book';
             }
 
@@ -64,3 +80,18 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  inputView: {
+    // backgroundColor: "powderblue",
+    borderWidth: 1,
+    borderColor:"powderblue",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    padding: 10,
+    fontSize: 20,
+    alignItems: "flex-start",
+  }
+});
